4652da171d612b13c8a0c9d5494be4a3167828c0
Your Name <you@example.com>
2018-05-28 10:39:02 +0700

    #home.html

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ItemSliding, Item } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ItemSliding, Item, ActionSheetController } from 'ionic-angular';
 import { StudyTypePage } from '../study-type/study-type';
 import { DatabaseProvider } from '../../providers/database/database';
 import {Study} from "../../model/Study";
@@ -24,6 +24,7 @@ export class HomePage {
   public listStudy : any;
   public listTemplate : any;
   activeItemSliding: ItemSliding = null;
+  public template: string;
 
   // Send Email
   public subject= '';
@@ -31,55 +32,62 @@ export class HomePage {
   public to ='';
   // public sort: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private homeProvider: HomeProvider, private database: DatabaseProvider, private utils: UtilitiesProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private homeProvider: HomeProvider, private database: DatabaseProvider,
+    private utils: UtilitiesProvider,
+    public actionSheetCtrl: ActionSheetController) {
       this.initHome();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
   }
+
   initHome(){
     this.listStudy = this.homeProvider.getListStudy();
     //let arrTemplate =  this.database.getListTemplate();
     this.listTemplate = this.database.getListTemplate();
     // this.sort = {TemplateName: 'Dozing'}
 
-    this.listTemplate.push({TemplateName: 'All'});
-    console.log('List Template 1: ', this.listTemplate)
+    this.listTemplate.unshift({TemplateName: 'All'});
+    console.log('List Template 1: ', this.listTemplate);
+
+    this.template = 'All';
 
   }
 
-  openOption(itemSlide: ItemSliding, item: Item, event) {
-    console.log('opening item slide..');
-    event.stopPropagation(); // here if you want item to be tappable
-    if (this.activeItemSliding) { // use this so that only one active sliding item allowed
-      this.closeOption();
-    }
-
-    this.activeItemSliding = itemSlide;
-    const swipeAmount = 70; // set your required swipe amount
-
-    console.log('swipe amount ', swipeAmount);
-    itemSlide.startSliding(swipeAmount);
-    itemSlide.moveSliding(swipeAmount);
-
-    itemSlide.setElementClass('active-slide', true);
-    itemSlide.setElementClass('active-options-right', true);
-    item.setElementStyle('transition', null);
-    item.setElementStyle('transform', 'translate3d(-' + swipeAmount + 'px, 0px, 0px)');
-  }
-
-  closeOption() {
-    console.log('closing item slide..');
-
-    if (this.activeItemSliding) {
-      this.activeItemSliding.close();
-      this.activeItemSliding = null;
-    }
-  }
-
-  onclickStudyType(){
-    this.navCtrl.push(StudyTypePage)
+  presentActionSheet(study, ev){
+    const actionSheet = this.actionSheetCtrl.create({
+      title: study.StudyName,
+      buttons: [
+        {
+          text: 'Delete',
+          handler: () => {
+            // this.moveDocumentModal(document);
+            this.deleteItem(study, ev);
+            console.log('Delete clicked');
+          }
+        }, {
+          text: 'Rename',
+          handler: () => {
+            const navTransition = actionSheet.dismiss();
+            navTransition.then(() => {
+              // wait until action sheet dismisses
+              // https://ionicframework.com/docs/api/components/action-sheet/ActionSheetController/#advanced
+              // this.renameDocument(document);
+              console.log('Rename clicked');
+            });
+            return false;
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 
   deleteItem(study,ev){
@@ -92,13 +100,12 @@ export class HomePage {
     this.listStudy = this.homeProvider.getStudyByTemplateName(templateName);
   }
 
-  // compareFn(option1: any, option2: any) {
-  //   return option1.value === option2.value;
-  // }
-
-
-  nextPageSettings(){
+  onclickSettings(){
     this.navCtrl.push(SettingsPage)
+  }
+
+  onclickNewStudy(){
+    this.navCtrl.push(StudyTypePage)
   }
 
   sendEmail(study,ev){
